f684b65579be15934990d814b38536377deb2b42
Jake Soucy <113120544+FreePirat@users.noreply.github.com>
2025-05-22 19:08:08 +0000

    Inheritance

--- app.py
+++ app.py
@@ -32,3 +32,27 @@ wordSet.addText('Hi, I\'m Jake! Here is a sentence I want to add')
 wordSet.addText('Here is another sentece I want to add.')
 
 print(wordSet.words)
+
+class Chihuahua(Dog):
+    def speak(self):
+        print(f'{self.name} says: Yap yap yap!')
+
+    def wagTail(self):
+        print('Vigorous wagging!')
+
+chihuahuaDog = Chihuahua('Roxy')
+chihuahuaDog.speak()
+chihuahuaDog.wagTail()
+
+class UniqueList(list):
+    def append(self, item):
+        if item in self:
+            return
+        super().append(item)
+
+uniqueList = UniqueList()
+uniqueList.append(1)
+uniqueList.append(2)
+uniqueList.append(2)
+
+print(uniqueList)
