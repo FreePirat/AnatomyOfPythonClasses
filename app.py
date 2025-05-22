0e66927d52d78182de6a10c6f7465ef3fae0d660
Jake Soucy <113120544+FreePirat@users.noreply.github.com>
2025-05-22 18:53:18 +0000

    WordSet Class

--- app.py
+++ app.py
@@ -12,3 +12,23 @@ class Dog:
 myDog = Dog('Rover')
 print(myDog.name)
 print(myDog.getLegs())
+
+class WordSet:
+    def __init__(self):
+        self.words = set()
+
+    def addText(self, text):
+        text = self.cleanText(text)
+        for word in text.split():
+            self.words.add(word)
+
+    def cleanText(self, text):
+        text = text.replace('!', '').replace('.', '').replace(',', '').replace('\'', '')
+        return text.lower()
+
+wordSet = WordSet()
+
+wordSet.addText('Hi, I\'m Jake! Here is a sentence I want to add')
+wordSet.addText('Here is another sentece I want to add.')
+
+print(wordSet.words)
